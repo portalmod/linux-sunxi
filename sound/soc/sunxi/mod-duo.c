edc2e3780dafb5b22da5038fd78c3b0de04a16ca
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2015-01-22 15:05:11 -0200

    * copyright info + overall cleanup & refactoring

--- sound/soc/sunxi/mod-duo.c
+++ sound/soc/sunxi/mod-duo.c
@@ -1,6 +1,7 @@
 /*
  * mod-duo.c  --  SoC audio for MOD Duo audio device
  *
+ * Copyright (c) 2015 Felipe Correa da Silva Sanches <juca@member.fsf.org>
  * Copyright (c) 2014 Rafael Guayer <rafael@musicaloperatingdevices.com>
  *
  * based on code from:
@@ -38,17 +39,18 @@
 #define INSTRUMENT 		0
 #define LINE 			1
 #define MICROPHONE 		2
-#define PAD_OFF			3
-#define PAD_ON			4
+#define STAGE_GAIN_OFF		3
+#define STAGE_GAIN_ON		4
 
 #define BYPASS 			0
 #define PROCESS 		1
 
-#define I2C_ADRESS	0b10011000	// 10011xx + R/!W
+#define TURN_SWITCH_ON 0
+#define TURN_SWITCH_OFF 1
+
+#define I2C_ADDRESS	0b10011000	// 10011xx + R/!W
 
 static int mod_duo_used = 0;
-
-// GPIO Handler
 static u32 mod_duo_gpio_handler = 0;
 
 #define MOD_DUO_GPIO_INIT(name)\
@@ -60,7 +62,7 @@ static u32 mod_duo_gpio_handler = 0;
 	gpio_set_one_pin_io_status(mod_duo_gpio_handler, PIN_DIR_OUT, "(name)");
 
 /*
-* GPIO Initialization - TODO: Function description.
+* GPIO Initialization
 */
 static int mod_duo_gpio_init(void)
 {
@@ -71,30 +73,30 @@ static int mod_duo_gpio_init(void)
 
 	mod_duo_gpio_handler = gpio_request_ex("mod_duo_soundcard_para", NULL);
 
-// JFET Switch A Pin Configuration
+	// JFET Switch A Pin Configuration
 	MOD_DUO_GPIO_INIT(jfet_sw_a1_pin)
 	MOD_DUO_GPIO_INIT(jfet_sw_a2_pin)
 	MOD_DUO_GPIO_INIT(jfet_sw_a3_pin)
 	MOD_DUO_GPIO_INIT(jfet_sw_a4_pin)
 
-// JFET Switch B Pin Configuration
+	// JFET Switch B Pin Configuration
 	MOD_DUO_GPIO_INIT(jfet_sw_b1_pin)
 	MOD_DUO_GPIO_INIT(jfet_sw_b2_pin)
 	MOD_DUO_GPIO_INIT(jfet_sw_b3_pin)
 	MOD_DUO_GPIO_INIT(jfet_sw_b4_pin)
 
-// Overflow Leds Pin Configuration
+	// Overflow Leds Pin Configuration
 	MOD_DUO_GPIO_INIT(led_ovfl1_pin)
 	MOD_DUO_GPIO_INIT(led_ovfl2_pin)
 	MOD_DUO_GPIO_INIT(led_ovfl3_pin)
 	MOD_DUO_GPIO_INIT(led_ovfl4_pin)
 
-// Headphone Volume Control Pin Configuration
-// TODO: Create a separate driver for Headphone Amplifier (LM4811).
+	// Headphone Volume Control Pin Configuration
+	// TODO: Create a separate driver for Headphone Amplifier (LM4811).
 	MOD_DUO_GPIO_INIT(hp_vol_pin)
 	MOD_DUO_GPIO_INIT(hp_clk_pin)
 
-// True Bypass Control Pin Configuration
+	// True Bypass Control Pin Configuration
 	MOD_DUO_GPIO_INIT(bypass_a_pin)
 	MOD_DUO_GPIO_INIT(bypass_b_pin)
 
@@ -102,9 +104,6 @@ static int mod_duo_gpio_init(void)
 	return 0;
 }
 
-/*
-* TODO: Function description.
-*/
 static void mod_duo_gpio_release(void)
 {
 	printk("[MOD Duo Machine Driver]Entered %s.\n", __func__);
@@ -113,65 +112,60 @@ static void mod_duo_gpio_release(void)
 	return;
 }
 
-/*
-* TODO: Function description.
-*/
 static void mod_duo_set_impedance(int channel, int type)
 {
-//	printk("[MOD Duo Machine Driver]Entered %s.\n", __func__);
 	switch(channel)
 	{
-		case CHANNEL_A:	// Channel A
+		case CHANNEL_A:
 		{
 			switch(type)
 			{
-				case INSTRUMENT:	// Instrument impedance
-					gpio_write_one_pin_value(mod_duo_gpio_handler, 1, "jfet_sw_a1_pin"); // 1: Switch is OFF
-					gpio_write_one_pin_value(mod_duo_gpio_handler, 1, "jfet_sw_a2_pin"); // 0: Switch is ON
+				case INSTRUMENT:
+					gpio_write_one_pin_value(mod_duo_gpio_handler, TURN_SWITCH_OFF, "jfet_sw_a1_pin");
+					gpio_write_one_pin_value(mod_duo_gpio_handler, TURN_SWITCH_OFF, "jfet_sw_a2_pin");
 					break;
-
-				case LINE:			// Line impedance
-					gpio_write_one_pin_value(mod_duo_gpio_handler, 0, "jfet_sw_a1_pin");
-					gpio_write_one_pin_value(mod_duo_gpio_handler, 1, "jfet_sw_a2_pin");
+				case LINE:
+					gpio_write_one_pin_value(mod_duo_gpio_handler, TURN_SWITCH_ON, "jfet_sw_a1_pin");
+					gpio_write_one_pin_value(mod_duo_gpio_handler, TURN_SWITCH_OFF, "jfet_sw_a2_pin");
 					break;
-				case MICROPHONE:	// Microphone impedance
-					gpio_write_one_pin_value(mod_duo_gpio_handler, 1, "jfet_sw_a1_pin");
-					gpio_write_one_pin_value(mod_duo_gpio_handler, 0, "jfet_sw_a2_pin");
+				case MICROPHONE:
+					gpio_write_one_pin_value(mod_duo_gpio_handler, TURN_SWITCH_OFF, "jfet_sw_a1_pin");
+					gpio_write_one_pin_value(mod_duo_gpio_handler, TURN_SWITCH_ON, "jfet_sw_a2_pin");
 					break;
-				case PAD_OFF:		// Gain Stage OFF
-					gpio_write_one_pin_value(mod_duo_gpio_handler, 0, "jfet_sw_a3_pin");
-					gpio_write_one_pin_value(mod_duo_gpio_handler, 1, "jfet_sw_a4_pin");
+				case STAGE_GAIN_OFF:
+					gpio_write_one_pin_value(mod_duo_gpio_handler, TURN_SWITCH_ON, "jfet_sw_a3_pin");
+					gpio_write_one_pin_value(mod_duo_gpio_handler, TURN_SWITCH_OFF, "jfet_sw_a4_pin");
 					break;
-				case PAD_ON:			// Gain Stage ON
-					gpio_write_one_pin_value(mod_duo_gpio_handler, 1, "jfet_sw_a3_pin");
-					gpio_write_one_pin_value(mod_duo_gpio_handler, 0, "jfet_sw_a4_pin");
+				case STAGE_GAIN_ON:
+					gpio_write_one_pin_value(mod_duo_gpio_handler, TURN_SWITCH_OFF, "jfet_sw_a3_pin");
+					gpio_write_one_pin_value(mod_duo_gpio_handler, TURN_SWITCH_ON, "jfet_sw_a4_pin");
 					break;
 			}
 			break;
 		}
-		case CHANNEL_B:	// Channel B
+		case CHANNEL_B:
 		{
 			switch(type)
 			{
-				case INSTRUMENT:	// Instrument impedance
-					gpio_write_one_pin_value(mod_duo_gpio_handler, 1, "jfet_sw_b1_pin"); // 1: Switch is OFF
-					gpio_write_one_pin_value(mod_duo_gpio_handler, 1, "jfet_sw_b2_pin"); // 0: Switch is ON
+				case INSTRUMENT:
+					gpio_write_one_pin_value(mod_duo_gpio_handler, TURN_SWITCH_OFF, "jfet_sw_b1_pin");
+					gpio_write_one_pin_value(mod_duo_gpio_handler, TURN_SWITCH_OFF, "jfet_sw_b2_pin");
 					break;
-				case LINE:			// Line impedance
-					gpio_write_one_pin_value(mod_duo_gpio_handler, 0, "jfet_sw_b1_pin");
-					gpio_write_one_pin_value(mod_duo_gpio_handler, 1, "jfet_sw_b2_pin");
+				case LINE:
+					gpio_write_one_pin_value(mod_duo_gpio_handler, TURN_SWITCH_ON, "jfet_sw_b1_pin");
+					gpio_write_one_pin_value(mod_duo_gpio_handler, TURN_SWITCH_OFF, "jfet_sw_b2_pin");
 					break;
-				case MICROPHONE:	// Microphone impedance
-					gpio_write_one_pin_value(mod_duo_gpio_handler, 1, "jfet_sw_b1_pin");
-					gpio_write_one_pin_value(mod_duo_gpio_handler, 0, "jfet_sw_b2_pin");
+				case MICROPHONE:
+					gpio_write_one_pin_value(mod_duo_gpio_handler, TURN_SWITCH_OFF, "jfet_sw_b1_pin");
+					gpio_write_one_pin_value(mod_duo_gpio_handler, TURN_SWITCH_ON, "jfet_sw_b2_pin");
 					break;
-				case PAD_OFF:		// Gain Stage OFF
-					gpio_write_one_pin_value(mod_duo_gpio_handler, 0, "jfet_sw_b3_pin");
-					gpio_write_one_pin_value(mod_duo_gpio_handler, 1, "jfet_sw_b4_pin");
+				case STAGE_GAIN_OFF:
+					gpio_write_one_pin_value(mod_duo_gpio_handler, TURN_SWITCH_ON, "jfet_sw_b3_pin");
+					gpio_write_one_pin_value(mod_duo_gpio_handler, TURN_SWITCH_OFF, "jfet_sw_b4_pin");
 					break;
-				case PAD_ON:			// Gain Stage ON
-					gpio_write_one_pin_value(mod_duo_gpio_handler, 1, "jfet_sw_b3_pin");
-					gpio_write_one_pin_value(mod_duo_gpio_handler, 0, "jfet_sw_b4_pin");
+				case STAGE_GAIN_ON:
+					gpio_write_one_pin_value(mod_duo_gpio_handler, TURN_SWITCH_OFF, "jfet_sw_b3_pin");
+					gpio_write_one_pin_value(mod_duo_gpio_handler, TURN_SWITCH_ON, "jfet_sw_b4_pin");
 					break;
 			}
 			break;
@@ -180,32 +174,26 @@ static void mod_duo_set_impedance(int channel, int type)
 	return;
 }
 
-/*
-* TODO: Function description.
-*/
-static void mod_duo_set_bypass(int channel, bool en)
+static void mod_duo_set_bypass(int channel, bool state)
 {
-//	printk("[MOD Duo Machine Driver]Entered %s.\n", __func__);
+// state == BYPASS:
+// 	No audio processing.
+//	Input is connected directly to output, bypassing the codec.
+//
+// state == PROCESS:
+// 	INPUT => CODEC => OUTPUT
+
 	switch(channel)
 	{
-		case CHANNEL_A:	// Channel A
-			if(en == PROCESS)	// Bypass OFF: Pin = HIGH - Processing. Input jack connected to Ccodec.
-				gpio_write_one_pin_value(mod_duo_gpio_handler, PROCESS, "bypass_a_pin");
-			else	// Bypass ON: Pin = LOW - Not processing. Input jack connected to output jack.
-				gpio_write_one_pin_value(mod_duo_gpio_handler, BYPASS, "bypass_a_pin");			
-			break;
-		case CHANNEL_B:	// Channel B
-			if(en == PROCESS)	// Bypass OFF: Pin = HIGH - Processing. Input jack connected to Ccodec.
-				gpio_write_one_pin_value(mod_duo_gpio_handler, PROCESS, "bypass_b_pin");
-			else	// Bypass ON: Pin = LOW - Not processing. Input jack connected to output jack.
-				gpio_write_one_pin_value(mod_duo_gpio_handler, BYPASS, "bypass_b_pin");
-			break;
+		case CHANNEL_A:
+			gpio_write_one_pin_value(mod_duo_gpio_handler, state, "bypass_a_pin"); break;
+		case CHANNEL_B:
+			gpio_write_one_pin_value(mod_duo_gpio_handler, state, "bypass_b_pin"); break;
 	}
 	return;
 }
 
 /* 
-* TODO: Function description.
 * TODO: Check if the _set_fmt and _setsysclk of cpu_dai and codec_dai should really be called here. 
 * TODO: I think the functions should be called on mod_duo_audio_init. 
 * TODO: This function is beeing called by aplay every time.
@@ -253,36 +241,24 @@ static int mod_duo_startup(struct snd_pcm_substream *substream)
 	return 0;
 }
 
-/* 
-* TODO: Function description. 
-*/
 static void mod_duo_shutdown(struct snd_pcm_substream *substream)
 {
 	printk("[MOD Duo Machine Driver]Entered %s.\n", __func__);
 	return;
 }
 
-/* 
-* TODO: Function description.
-*/
 static int mod_duo_analog_suspend(struct snd_soc_card *card)
 {
 	printk("[MOD Duo Machine Driver]Entered %s.\n", __func__);
 	return 0;
 }
 
-/* 
-* TODO: Function description.
-*/
 static int mod_duo_analog_resume(struct snd_soc_card *card)
 {
 	printk("[MOD Duo Machine Driver]Entered %s.\n", __func__);
 	return 0;
 }
 
-/* 
-* TODO: Function description.
-*/
 static int mod_duo_hw_params(struct snd_pcm_substream *substream,
 				     struct snd_pcm_hw_params *params)
 {
@@ -315,7 +291,6 @@ static int mod_duo_hw_params(struct snd_pcm_substream *substream,
 }
 
 /*
-* TODO: Function description.
 * Implemented to replace the calls made in mod_duo_startup.
 * The mod_duo_startup and then cpu_dai and codec_dai *_set_fmt and *_set_sysclk were called every aplay, and the reconfiguration was redundant.
 */
@@ -327,22 +302,28 @@ int mod_duo_dai_link_init(struct snd_soc_pcm_runtime *rtd)
 	unsigned int fmt = 0;
 	unsigned int mclk = 24576000;	// MOD Duo Sound Card has an 2457600Hz external clock
 	printk("[MOD Duo Machine Driver]Entered %s.\n", __func__);
+
 	// Configure the CS4245 Codec Driver for MOD Duo Sound Card
 	fmt = 	SND_SOC_DAIFMT_I2S |	/* I2S mode */
     	 	SND_SOC_DAIFMT_CBM_CFS;	// CS4245: DAC master ADC slave (ALSA: codec clk master & frame slave).
+
 	//call cs4245_set_dai_fmt (CS4245 Codec Driver) - CODEC DAI.
 	ret = snd_soc_dai_set_fmt(codec_dai, fmt);	// Calls snd_soc_dai_driver .ops->set_fmt
 	if (ret < 0)
 		return ret;
+
 	//call cs4245_set_dai_sysclk (CS4245 Codec Driver) - CODEC DAI.
 	ret = snd_soc_dai_set_sysclk(codec_dai, CS4245_MCLK1_SET , mclk, 0);
 	if (ret < 0)
 		return ret;
+
+
 	// Configure the I2S Plataform Driver for MOD Duo Sound Card
 	fmt = 	SND_SOC_DAIFMT_CBS_CFS |					// SoC clk & frm slave.
 			SND_SOC_DAIFMT_I2S |						// I2S mode
 			SND_SOC_DAIFMT_SUNXI_IISFAT0_WSS_32BCLK |	// Word Size = 32.
 			SND_SOC_DAIFMT_NB_NF;						// normal bit clock + frame.
+
 	//call sunxi_i2s_set_fmt (I2S Plataform Driver)- CPU DAI.
 	ret = snd_soc_dai_set_fmt(cpu_dai, fmt);
 	if (ret < 0)
@@ -350,19 +331,13 @@ int mod_duo_dai_link_init(struct snd_soc_pcm_runtime *rtd)
 	return ret;
 }
 
-/* 
-* TODO: Structure description.
-*/
 static struct snd_soc_ops mod_duo_ops = {
 	.startup = mod_duo_startup,
 	.shutdown = mod_duo_shutdown,
 	.hw_params = mod_duo_hw_params,
 };
 
-/* 
-* TODO: Structure description.
-*/
-static struct snd_soc_dai_link snd_soc_mod_duo_dai =
+static struct snd_soc_dai_link snd_soc_mod_duo_dai_link =
 {
 	.name = "MOD-DUO-I2S",
 	.stream_name = "MOD-DUO-SUNXI-I2S",
@@ -374,21 +349,15 @@ static struct snd_soc_dai_link snd_soc_mod_duo_dai =
 	.init = &mod_duo_dai_link_init,
 };
 
-/* 
-* TODO: Structure description.
-*/
 static struct snd_soc_card snd_soc_mod_duo_soundcard = {
 	.name = "MOD-Duo-Sound-Card",
 	.owner = THIS_MODULE,
-	.dai_link = &snd_soc_mod_duo_dai,
+	.dai_link = &snd_soc_mod_duo_dai_link,
 	.num_links = 1,
 	.suspend_post = mod_duo_analog_suspend,
 	.resume_pre	= mod_duo_analog_resume,
 };
 
-// /*
-// * TODO: Function description.
-// */
 // static int __devinit mod_duo_probe(struct platform_device *pdev)
 // {
 // 	printk("[MOD Duo Machine Driver]Entered %s.\n", __func__);
@@ -397,9 +366,6 @@ static struct snd_soc_card snd_soc_mod_duo_soundcard = {
 // 	return snd_soc_register_card(&snd_soc_mod_duo_soundcard);
 // }
 
-// /*
-// * TODO: Function description.
-// */
 // static int __devexit mod_duo_remove(struct platform_device *pdev)
 // {
 // 	printk("[MOD Duo Machine Driver]Entered %s.\n", __func__);
@@ -408,9 +374,6 @@ static struct snd_soc_card snd_soc_mod_duo_soundcard = {
 // 	return 0;
 // }
 
-/*
-* TODO: Structure description.
-*/
 static struct platform_device *mod_duo_audio_device;
 // static struct platform_device mod_duo_audio_device = {
 // 	.name = "MOD-Duo-Sound-Card",
@@ -429,7 +392,6 @@ static struct platform_device *mod_duo_audio_device;
 // };
 
 /*
-* TODO: Function description.
 * When loading the module, the kernel sends a message saying to use snd_soc_register_card()
 * "soc-audio soc-audio.0: ASoC machine MOD-Duo-Sound-Card should use snd_soc_register_card()"
 */
@@ -461,8 +423,8 @@ static int __init mod_duo_audio_init(void)
 	}
 	if(mod_duo_used) {
 		mod_duo_gpio_init();
-		mod_duo_set_impedance(CHANNEL_A, PAD_OFF);
-		mod_duo_set_impedance(CHANNEL_B, PAD_OFF);
+		mod_duo_set_impedance(CHANNEL_A, STAGE_GAIN_OFF);
+		mod_duo_set_impedance(CHANNEL_B, STAGE_GAIN_OFF);
 		mod_duo_set_impedance(CHANNEL_A, INSTRUMENT);
 		mod_duo_set_impedance(CHANNEL_B, INSTRUMENT);
 		mod_duo_set_bypass(CHANNEL_A, PROCESS);
@@ -471,9 +433,6 @@ static int __init mod_duo_audio_init(void)
 	return 0;
 }
 
-/*
-*
-*/
 static void __exit mod_duo_audio_exit(void)
 {
 	printk("[MOD Duo Machine Driver]Entered %s.\n", __func__);
@@ -489,7 +448,6 @@ static void __exit mod_duo_audio_exit(void)
 module_init(mod_duo_audio_init);
 module_exit(mod_duo_audio_exit);
 
-/* Module information */
-MODULE_AUTHOR("Rafael Guayer <rafael@musicaloperatingdevices.com>");
+MODULE_AUTHOR("Felipe Sanches <juca@members.fsf.org>, Rafael Guayer <rafael@musicaloperatingdevices.com>");
 MODULE_DESCRIPTION("MOD Duo Sound Card Audio Machine Driver");
 MODULE_LICENSE("GPL");

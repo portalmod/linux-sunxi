c69eb3b4e103cb994ea83b4f2d3a509e83af0aaa
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2015-01-22 15:25:28 -0200

    code-style / tab / white-space cleanup

--- sound/soc/sunxi/mod-duo.c
+++ sound/soc/sunxi/mod-duo.c
@@ -283,7 +283,7 @@ static int mod_duo_analog_resume(struct snd_soc_card *card)
 }
 
 static int mod_duo_hw_params(struct snd_pcm_substream *substream,
-				     struct snd_pcm_hw_params *params)
+							 struct snd_pcm_hw_params *params)
 {
 	struct snd_soc_pcm_runtime *rtd = substream->private_data;
 	struct snd_soc_dai *codec_dai = rtd->codec_dai;

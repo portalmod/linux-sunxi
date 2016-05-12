69c32140ad886efa45a22588d31be8628c498acf
falkTX <falktx@gmail.com>
2016-05-12 11:42:19 +0200

    Minor case reorder

--- sound/soc/sunxi/i2s/sunxi-i2sdma.c
+++ sound/soc/sunxi/i2s/sunxi-i2sdma.c
@@ -327,8 +327,8 @@ static int sunxi_pcm_trigger(struct snd_pcm_substream *substream, int cmd)
 		case SNDRV_PCM_TRIGGER_PAUSE_RELEASE:
 			sunxi_dma_start(prtd->params);
 			break;
-		case SNDRV_PCM_TRIGGER_SUSPEND:
 		case SNDRV_PCM_TRIGGER_STOP:
+		case SNDRV_PCM_TRIGGER_SUSPEND:
 		case SNDRV_PCM_TRIGGER_PAUSE_PUSH:
 			sunxi_dma_stop(prtd->params);
 			break;
